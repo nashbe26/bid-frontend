629ac2e01cd685f157b7fe869438bf1aa276ef70
hatem782 <hatembenechikh100@gmail.com>
2023-12-02 23:39:44 +0100

    auctions home

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -6,7 +6,7 @@ import heart_empty from "./icons/heart_emty.svg";
 import heart_fill from "./icons/heart_fill.svg";
 import blue_comp from "./icons/blue_comp.svg";
 import blue_ppl from "./icons/blue_ppl.svg";
-
+import clock from "./icons/Clock.svg";
 import left_normal_arrow from "./arrows/left_normal.svg";
 import right_normal_arrow from "./arrows/right_normal.svg";
 
@@ -21,4 +21,5 @@ export {
   heart_fill,
   blue_comp,
   blue_ppl,
+  clock,
 };
--- src/components/Cards/CardAuction/CardAuction.jsx
+++ src/components/Cards/CardAuction/CardAuction.jsx
@@ -5,8 +5,9 @@ import {
   heart_empty,
   blue_comp,
   blue_ppl,
+  clock,
 } from "../../../assets/svgs";
-import { P14, P18, P19 } from "../../TXT/TXT";
+import { P14, P17, P18, P19 } from "../../TXT/TXT";
 import Flex from "../../Flex/Flex";
 import Button from "../../Buttons/Button";
 import { sold_out } from "../../../assets/images";
@@ -25,65 +26,95 @@ const empty_auction = {
   starting_price: "1.77",
   with_icon: "company",
   end_date: new Date(),
+  end_soon: false,
   finis_price: "13",
   hearted: false,
 };
 
 const CardAuction = ({ auction = empty_auction }) => {
   let styles_state = auction.live ? styles.live : styles.closed;
-  let live = auction.live ? "Live" : "Closed";
-  let end_price = auction.live ? "Starting price" : "Finised price";
-  let heart = auction.hearted ? heart_fill : heart_empty;
-  let cercle_item = c_type[auction.with_icon] || null;
 
   return (
     <div className={`${styles.auction} ${styles_state}`}>
-      <Flex flex="between" className={styles.head}>
-        <P18> {live} </P18>
-        <img src={heart} alt="" className={styles.icon} />
-      </Flex>
+      <Header auction={auction} />
+      <Image auction={auction} />
+      <EndSoon auction={auction} />
+      <Content auction={auction} />
+    </div>
+  );
+};
 
-      <Flex flex="center" className={styles.auction_img}>
-        <div className={styles.img}>
-          <img src={auction.img} alt="" />
-        </div>
+const Header = ({ auction = empty_auction }) => {
+  let live = auction.live ? "Live" : "Closed";
+  let heart = auction.hearted ? heart_fill : heart_empty;
 
-        <div className={styles.overlay}>
-          {cercle_item && (
-            <Flex flex="center" className={styles.cercle_item}>
-              <img src={cercle_item} alt="" />
-            </Flex>
-          )}
-        </div>
+  return (
+    <Flex flex="between" className={styles.head}>
+      <P18> {live} </P18>
+      <img src={heart} alt="" className={styles.icon} />
+    </Flex>
+  );
+};
 
-        {!auction.live && (
-          <Flex flex="center" className={styles.overlay_closed}>
-            <img src={sold_out} alt="sold out" />
+const Image = ({ auction = empty_auction }) => {
+  let cercle_item = c_type[auction.with_icon] || null;
+
+  return (
+    <Flex flex="center" className={styles.auction_img}>
+      <div className={styles.img}>
+        <img src={auction.img} alt="" />
+      </div>
+
+      <div className={styles.overlay}>
+        {auction.live && cercle_item && (
+          <Flex flex="center" className={styles.cercle_item}>
+            <img src={cercle_item} alt="" />
           </Flex>
         )}
+      </div>
+
+      {!auction.live && (
+        <Flex flex="center" className={styles.overlay_closed}>
+          <img src={sold_out} alt="sold out" />
+        </Flex>
+      )}
+    </Flex>
+  );
+};
+
+const EndSoon = ({ auction = empty_auction }) => {
+  return auction.end_soon ? (
+    <Flex flex="center" className={styles.finish_in}>
+      <img src={clock} alt="" />
+      <P17 weight={700}>Finish in : 2 min</P17>
+    </Flex>
+  ) : null;
+};
+
+const Content = ({ auction = empty_auction }) => {
+  let end_price = auction.live ? "Starting price" : "Finised price";
+
+  return (
+    <div className={styles.content}>
+      <Flex flex="between" className={styles.title_and_price}>
+        <P19 weight={700} className={styles.title}>
+          {auction.title}
+        </P19>
+        <P18 weight={700} className={styles.price}>
+          {auction.price}$
+        </P18>
       </Flex>
 
-      <div className={styles.content}>
-        <Flex flex="between" className={styles.title_and_price}>
-          <P19 weight={700} className={styles.title}>
-            {auction.title}
-          </P19>
-          <P18 weight={700} className={styles.price}>
-            {auction.price}$
-          </P18>
-        </Flex>
+      <P14 className={styles.name_bid}>{auction.name_bid} bid</P14>
+      <P14 className={styles.starting_price}>
+        {end_price} : {auction.starting_price}$
+      </P14>
 
-        <P14 className={styles.name_bid}>{auction.name_bid} bid</P14>
-        <P14 className={styles.starting_price}>
-          {end_price} : {auction.starting_price}$
-        </P14>
-
-        {auction.live && (
-          <Button type="fill" fullWidth>
-            Join Biding
-          </Button>
-        )}
-      </div>
+      {auction.live && (
+        <Button type="fill" fullWidth>
+          Join Biding
+        </Button>
+      )}
     </div>
   );
 };
